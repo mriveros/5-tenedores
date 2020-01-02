bd742d01b224b68f5bcd91b49335563d6f8285a1
Marcos Riveros <mriveros.py@gmail.com>
2020-01-02 13:53:57 -0300

    adding Loading components

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -3,16 +3,20 @@ import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 import { validateEmail } from "../../utils/Validation";
 import * as firebase from "firebase";
+import { withNavigation } from "react-navigation";
+import Loading from "../Loading";
 
-export default function RegisterForm(props) {
-  const { toastRef } = props;
+function RegisterForm(props) {
+  const { toastRef, navigation } = props;
   const [hidePassword, setHidePassword] = useState(true);
   const [hideRepeatPassword, setHideRepeatPassword] = useState(true);
+  const [isVisibleLoading, setIsVisibleLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
 
   const register = async () => {
+    setIsVisibleLoading(true);
     if (!email || !password || !repeatPassword) {
       toastRef.current.show("Todos los campos son obligatorios");
     } else {
@@ -27,6 +31,7 @@ export default function RegisterForm(props) {
             .createUserWithEmailAndPassword(email, password)
             .then(() => {
               toastRef.current.show("Usuario Creado correctamente");
+              navigation.navigate("MyAccount");
             })
             .catch(() => {
               toastRef.current.show(
@@ -36,6 +41,7 @@ export default function RegisterForm(props) {
         }
       }
     }
+    setIsVisibleLoading(false);
   };
   return (
     <View style={styles.formContainer} behavior="padding" enabled>
@@ -89,9 +95,12 @@ export default function RegisterForm(props) {
         buttonStyle={styles.btnRegister}
         onPress={register}
       />
+      <Loading text="Creando Cuenta" isVisible={isVisibleLoading} />
     </View>
   );
 }
+
+export default withNavigation(RegisterForm);
 
 const styles = StyleSheet.create({
   formContainer: {
