f14ba76b12fcd783b549262008fe936d5bb6570f
Marcos Riveros <mriveros.py@gmail.com>
2020-01-02 14:27:34 -0300

    adding login form

---
+++ app/components/Account/LoginForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Input, Icon, Button } from "react-native-elements";
+
+export default function LoginForm() {
+  const [hidePassword, setHidePassword] = useState(true);
+  const login = () => {
+    console.log("usuario logueado..");
+  };
+
+  return (
+    <View style={styles.formContainer}>
+      <Input
+        placeholder="Correo Electrónico"
+        containerStyle={styles.inputForm}
+        onChange={() => console.log("Email actualizado..")}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="at"
+            iconStyle={styles.iconRight}
+          />
+        }
+      />
+      <Input
+        placeholder="Contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={hidePassword}
+        onChange={() => console.log("contraseña actualizado..")}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={hidePassword ? "eye-outline" : "eye-off-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setHidePassword(!hidePassword)}
+          />
+        }
+      />
+      <Button
+        title="Iniciar Sesión"
+        containerStyle={styles.btnContainerLogin}
+        buttonStyle={styles.btnLogin}
+        onPress={login}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30
+  },
+  inputForm: {
+    width: "100%",
+    marginTop: 20
+  },
+  iconRight: {
+    color: "#c1c1c1"
+  },
+  btnContainerLogin: {
+    marginTop: 20,
+    width: "95%"
+  },
+  btnLogin: {
+    backgroundColor: "#00a680"
+  }
+});
