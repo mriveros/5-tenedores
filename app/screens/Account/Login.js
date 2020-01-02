4f7151287bec774500d236f23f1184ae6851d089
Marcos Riveros <mriveros.py@gmail.com>
2020-01-02 14:27:00 -0300

    adding login form

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from "React";
 import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
 import { Divider } from "react-native-elements";
+import LoginForm from "../../components/Account/LoginForm";
 
 export default function Login(props) {
   const { navigation } = props;
@@ -12,7 +13,7 @@ export default function Login(props) {
         resizeMode="contain"
       />
       <View style={styles.viewContainer}>
-        <Text>Form Login...</Text>
+        <LoginForm />
         <CreateAccount navigation={navigation} />
       </View>
       <Divider style={styles.divider} />
