2293c8d2f2b198202fdd3b8245078ee3ad03f29f
Marcos Riveros <mriveros.py@gmail.com>
2020-01-09 15:23:48 -0300

    Adding avatar

---
+++ app/components/Account/InfoUser.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Avatar } from "react-native-elements";
+
+export default function InfoUser(props) {
+  const {
+    userInfo,
+    userInfo: { photoURL, uid, email, displayName }
+  } = props;
+
+  console.log(userInfo);
+
+  const changeAvatar = () => {
+    console.log("cambiando avatar");
+  };
+
+  return (
+    <View style={styles.viewUserInfo}>
+      <Avatar
+        rounded
+        size="large"
+        showEditButton
+        onEditPress={changeAvatar}
+        containerStyle={styles.userInfoAvatar}
+        source={{
+          uri: photoURL
+            ? photoURL
+            : "https://api.adorable.io/avatars/100/abott@adorable.png"
+        }}
+      />
+      <View>
+        <Text style={styles.displayName}>
+          {displayName ? displayName : "Anónimo"}
+        </Text>
+        <Text>{email ? email : "Social Login"}</Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  viewUserInfo: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#f2f2f2",
+    paddingTop: 30,
+    paddingBottom: 30
+  },
+  userInfoAvatar: {
+    marginRight: 20
+  },
+  displayName: {
+    fontWeight: "bold"
+  }
+});
--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Button } from "react-native-elements";
 import * as firebase from "firebase";
+import InfoUser from "../../components/Account/InfoUser";
 
 export default function UserLogged() {
+  const [userInfo, setUserInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const user = await firebase.auth().currentUser;
+      setUserInfo(user.providerData[0]);
+      console.log(user);
+    })();
+  }, []);
   return (
     <View>
-      <Text>UserLogged...</Text>
+      <InfoUser userInfo={userInfo} />
       <Button title="Cerrar Sesión" onPress={() => firebase.auth().signOut()} />
     </View>
   );
