41699cd6ac3f9bccaac483271f5b66404db3582e
Marcos Riveros <mriveros.py@gmail.com>
2020-02-25 12:04:24 -0300

    adding list restaurants

---
+++ app/components/Restaurants/ListRestaurants.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ListsRestaurants(props) {
+  const restaurants = props;
+
+  console.log(restaurants);
+
+  return (
+    <View>
+      <Text>ListRestaurants...</Text>
+    </View>
+  );
+}
--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ActionButton from "react-native-action-button";
+import ListRestaurants from "../../components/Restaurants/ListRestaurants";
 
 import { firebaseApp } from "../../utils/FireBase";
 import firebase from "firebase/app";
@@ -10,15 +11,45 @@ const db = firebase.firestore(firebaseApp);
 export default function Restaurants(props) {
   const { navigation } = props;
   const [user, setUser] = useState(null);
+  const [restaurants, setRestaurants] = useState([]);
+  const [startRestaurants, setStartRestaurants] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalRestaurants, setTotalRestaurants] = useState(0);
+  const limitRestaurants = 8;
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(userInfo => {
       setUser(userInfo);
     });
   }, []);
+
+  useEffect(() => {
+    db.collection("restaurants")
+      .get()
+      .then(snap => {
+        setTotalRestaurants(snap.size);
+      });
+    (async () => {
+      let resultRestaurants = [];
+      const restaurants = db
+        .collection("restaurants")
+        .orderBy("createAt", "desc")
+        .limit(limitRestaurants);
+      await restaurants.get().then(response => {
+        setStartRestaurants(response.docs[response.docs.length - 1]);
+        response.forEach(doc => {
+          let restaurant = doc.data();
+          restaurant.id = doc.id;
+          resultRestaurants.push({ restaurant });
+        });
+
+        setRestaurants(resultRestaurants);
+      });
+    })();
+  }, []);
   return (
     <View style={styles.viewBody}>
-      <Text> Estamos en Restaurantes.</Text>
+      <ListRestaurants restaurats={restaurants} />
       {user && <AddRestaurantButton navigation={navigation} />}
     </View>
   );
